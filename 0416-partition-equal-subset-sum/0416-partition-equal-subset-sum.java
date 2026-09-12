2614e6b6ba6fb3a5da358b21d535a1c41e432a61
Debjitsutar <debjitsutar65@gmail.com>
2026-09-12 22:30:00 +0530

    Time: 64 ms (39.45%), Space: 47.5 MB (59.92%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,26 +6,19 @@ class Solution {
 
         int target=sum/2;
         if(sum%2!=0) return false;
-        int dp[][]=new int[n][target+1];
-        for(int i=0;i<n;i++)
+        boolean dp[][]=new boolean[n+1][target+1];
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++)
         {
-            java.util.Arrays.fill(dp[i],-1);
+            for(int j=0;j<=target;j++)
+            {
+                dp[i][j]=dp[i-1][j];
+                if(j>=nums[i-1])
+                {
+                    dp[i][j]=dp[i][j]||dp[i-1][j-nums[i-1]];
+                }
+            }
         }
-        return solve(nums,n-1,target,dp);
-    }
-    public static boolean solve(int nums[],int index,int target,int dp[][])
-    {
-        if(target==0) return true;
-        if(index<0) return false;
-        if(dp[index][target]!=-1) return dp[index][target]==1;
-        boolean notTake=solve(nums,index-1,target,dp);
-        boolean take=false;
-        if(target>=nums[index])
-        {
-            take=solve(nums,index-1,target-nums[index],dp);
-        }
-        boolean res=take||notTake;
-        dp[index][target]=res?1:0;
-        return res;
+        return dp[n][target];
     }
 }
