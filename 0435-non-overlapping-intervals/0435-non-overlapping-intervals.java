fdedb198cb948125a159657c38a585f6958123e4
Debjitsutar <debjitsutar65@gmail.com>
2026-09-14 22:04:29 +0530

    Time: 46 ms (73.33%), Space: 115.9 MB (38.06%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -3,13 +3,11 @@ class Solution {
         Arrays.sort(intervals, (a,b)-> a[1]-b[1]);
 
         int c=0;
-
-        int previousEnd=intervals[0][1];
-
+        int end=intervals[0][1];
         for(int i=1;i<intervals.length;i++)
         {
-            if(intervals[i][0]<previousEnd) c++;
-            else previousEnd=intervals[i][1];
+            if(intervals[i][0]<end) c++;
+            else end=intervals[i][1];
         }
         return c;
     }
